095e03656b385e63c6b587e91c33d1c9e0fc08f4
Yordan Boev <iordan.boev@gmail.com>
2012-10-24 17:29:29 +0200

    Changed eclipse settings to use spaces only instead of tabs in order to fix checkstyle errors.

--- src/main/java/org/jenkinsci/plugins/PeriodicReincarnation.java
+++ src/main/java/org/jenkinsci/plugins/PeriodicReincarnation.java
@@ -31,213 +31,227 @@ import hudson.util.RunList;
 /**
  * Main class of the periodic reincarnation plug-in. Method execute is called
  * every minute but further functionality and restart of failed jobs happens
- * only when the time specified in the cron tab overlaps with the current minute.
- *   
+ * only when the time specified in the cron tab overlaps with the current
+ * minute.
+ * 
  */
 @Extension
 public class PeriodicReincarnation extends AsyncPeriodicWork {
 
-	private static final Logger LOGGER = Logger
-			.getLogger(PeriodicReincarnation.class.getName());
+    private static final Logger LOGGER = Logger
+            .getLogger(PeriodicReincarnation.class.getName());
 
-	/**
-	 * Constructor.
-	 */
-	public PeriodicReincarnation() {
-		super("PeriodicReincarnation");
-	}
+    /**
+     * Constructor.
+     */
+    public PeriodicReincarnation() {
+        super("PeriodicReincarnation");
+    }
 
-	/**
-	 * This method is called every minute.
-	 */
-	@Override
-	protected void execute(TaskListener taskListener) {
-		ReincarnateFailedJobsConfiguration config = ReincarnateFailedJobsConfiguration
-				.get();
-		if (config == null) {
-			LOGGER.info("No configuration available.");
-			return;
-		}
+    /**
+     * This method is called every minute.
+     */
+    @Override
+    protected void execute(TaskListener taskListener) {
+        ReincarnateFailedJobsConfiguration config = ReincarnateFailedJobsConfiguration
+                .get();
+        if (config == null) {
+            LOGGER.info("No configuration available.");
+            return;
+        }
 
-		String cron = config.getCronTime();
-		boolean isActive = config.isActive();
-		if (!isActive) {
-			return;
-		}
-		if (cron != null) {
-			try {
+        String cron = config.getCronTime();
+        boolean isActive = config.isActive();
+        if (!isActive) {
+            return;
+        }
+        if (cron != null) {
+            try {
 
-				CronTab cronTab = new CronTab(cron);
-				long currentTime = System.currentTimeMillis();
+                CronTab cronTab = new CronTab(cron);
+                long currentTime = System.currentTimeMillis();
 
-				if ((cronTab.ceil(currentTime).getTimeInMillis() - currentTime) == 0
-						&& isActive) {
+                if ((cronTab.ceil(currentTime).getTimeInMillis() - currentTime) == 0
+                        && isActive) {
 
-					List<?> projectList = Hudson.getInstance()
-							.getProjects();
-					for (Iterator<?> i = projectList.iterator(); i
-							.hasNext();) {
-						Project<?, ?> project = (Project<?, ?>) i.next();
-						if (project != null
-								&& project instanceof BuildableItem
-								&& project.getLastBuild() != null
-								&& project.getLastBuild().getResult() != null
-								&& project.getLastBuild().getResult()
-										.isWorseOrEqualTo(Result.FAILURE)
-								&& !project.isBuilding()
-								&& !project.isInQueue()) {
-							if (checkRegExprs(project.getLastBuild())) {
-								this.restart(project, config, "regEx hit");
-							} else if (config.isRestartUnchangedJobsEnabled()
-									&& qualifyForUnchangedRestart(project)) {
-								this.restart(project, config, "no change since success");
-							}
-						}
+                    List<?> projectList = Hudson.getInstance().getProjects();
+                    for (Iterator<?> i = projectList.iterator(); i.hasNext();) {
+                        Project<?, ?> project = (Project<?, ?>) i.next();
+                        if (project != null
+                                && project instanceof BuildableItem
+                                && project.getLastBuild() != null
+                                && project.getLastBuild().getResult() != null
+                                && project.getLastBuild().getResult()
+                                        .isWorseOrEqualTo(Result.FAILURE)
+                                && !project.isBuilding()
+                                && !project.isInQueue()) {
+                            if (checkRegExprs(project.getLastBuild())) {
+                                this.restart(project, config, "regEx hit");
+                            } else if (config.isRestartUnchangedJobsEnabled()
+                                    && qualifyForUnchangedRestart(project)) {
+                                this.restart(project, config,
+                                        "no change since success");
+                            }
+                        }
 
-					}
-				}
+                    }
+                }
 
-			} catch (ANTLRException e) {
-				LOGGER.warning("Could not parse the given cron tab. Check for type errors: "
-						+ e.getMessage());
-			}
-		} else {
-			LOGGER.warning("Cron time is not configured.");
-		}
-	}
+            } catch (ANTLRException e) {
+                LOGGER.warning("Could not parse the given cron tab. Check for type errors: "
+                        + e.getMessage());
+            }
+        } else {
+            LOGGER.warning("Cron time is not configured.");
+        }
+    }
 
-	/**
-	 * Recurrence will occur every minute, but action will be taken
-	 * according to the cron time set in the configuration. Only
-	 * when this minute and the cron tab time overlap.
-	 */
-	@Override
-	public long getRecurrencePeriod() {
-		return MIN;
-	}
-	
-	
-	/**
-	 * Returns this AsyncTask.
-	 * @return the instance of PeriodicReincarnation which is currently running
-	 */
-	public static PeriodicReincarnation get() {
-		return AsyncPeriodicWork.all().get(PeriodicReincarnation.class);
-	}
-	
-	/**
-	 * Determines whether or not there were changes between two builds.
-	 * @param build1 First build.
-	 * @param build2 Second build.
-	 * @return true if there is at least one chage, false otherwise.
-	 */
-	private boolean changesBetweenTwoBuilds(Run<?, ?> build1, Run<?, ?> build2) {
-		return ((AbstractBuild<?, ?>) build1).getChangeSet().equals(((AbstractBuild<?, ?>) build2).getChangeSet());
-	}
+    /**
+     * Recurrence will occur every minute, but action will be taken according to
+     * the cron time set in the configuration. Only when this minute and the
+     * cron tab time overlap.
+     */
+    @Override
+    public long getRecurrencePeriod() {
+        return MIN;
+    }
 
-	/**
-	 * If there were no changes between the last 2 builds of a project and
-	 * the last build failed but the previous didn't, then this project is
-	 * being restarted if this unchanged restart option is enabled.
-	 * @param project the project.
-	 * @return true if it qualifies, false otherwise.
-	 */
-	private boolean qualifyForUnchangedRestart(Project<?, ?> project) {
-		// proves if a build WAS stable and if there are changes between the last two builds.
-		// WAS stable means: last build was worse or equal to FAILURE, second last was better than FAILURE 
-		if (project.getBuilds() != null && project.getBuilds().size() >= 2) {
-			RunList<?> builds =  project.getBuilds();
-			Run<?, ?> lastBuild = builds.getLastBuild();
-			Run<?, ?> secondLastBuild = builds.get(builds.size() - 2);
-			if (lastBuild != null && lastBuild.getResult() != null
-					&& lastBuild.getResult().isWorseOrEqualTo(Result.FAILURE)
-					&& secondLastBuild != null
-					&& secondLastBuild.getResult() != null
-					&& secondLastBuild.getResult().isBetterThan(Result.FAILURE)
-					&& !changesBetweenTwoBuilds(lastBuild, secondLastBuild)) {
-				// last build failed, but second one didn't and there were no changes between the two builds
-				// in this case we restart the build
-				return true;
-			}
-			// last build was not a failure or 2nd last was.
-			// no restart
-			return false;
-		}
-		// less than 2 builds
-		// no restart
-		return false;
-	}
+    /**
+     * Returns this AsyncTask.
+     * 
+     * @return the instance of PeriodicReincarnation which is currently running
+     */
+    public static PeriodicReincarnation get() {
+        return AsyncPeriodicWork.all().get(PeriodicReincarnation.class);
+    }
 
-	/**
-	 * Helper method for restarting a project.
-	 * @param project the project.
-	 * @param config instance of periodic reincarnation configuration.
-	 */
-	private void restart(Project<?, ?> project,
-			ReincarnateFailedJobsConfiguration config, String cause) {
-		project.scheduleBuild(new ReincarnateFailedBuildsCause());
-		if (config.isLogInfoEnabled()) {
-			
-			LOGGER.info("Restarting failed build(" + cause + ")........"
-					+ project.getDisplayName());
-		}
-	}
+    /**
+     * Determines whether or not there were changes between two builds.
+     * 
+     * @param build1
+     *            First build.
+     * @param build2
+     *            Second build.
+     * @return true if there is at least one chage, false otherwise.
+     */
+    private boolean changesBetweenTwoBuilds(Run<?, ?> build1, Run<?, ?> build2) {
+        return ((AbstractBuild<?, ?>) build1).getChangeSet().equals(
+                ((AbstractBuild<?, ?>) build2).getChangeSet());
+    }
 
-	/**
-	 * Checks if a certain build matches any of the given regular expressions.
-	 * @param build the build.
-	 * @return true if at least one match, false otherwise.
-	 */
-	private boolean checkRegExprs(Run<?, ?> build) {
-		ReincarnateFailedJobsConfiguration config = new ReincarnateFailedJobsConfiguration();
-		List<RegEx> regExprs = config.getRegExprs();
-		if (regExprs == null || regExprs.size() == 0)
-			return true;
-		for (Iterator<RegEx> i = regExprs.iterator(); i.hasNext();) {
-			RegEx currentRegEx = i.next();
-			try {
-				if (checkFile(build.getLogFile(), currentRegEx.getPattern(),
-						true)) {
-					return true;
-				}
-			} catch (AbortException e) {
-				// e.printStackTrace();
-			}
-		}
-		return false;
-	}
+    /**
+     * If there were no changes between the last 2 builds of a project and the
+     * last build failed but the previous didn't, then this project is being
+     * restarted if this unchanged restart option is enabled.
+     * 
+     * @param project
+     *            the project.
+     * @return true if it qualifies, false otherwise.
+     */
+    private boolean qualifyForUnchangedRestart(Project<?, ?> project) {
+        // proves if a build WAS stable and if there are changes between the
+        // last two builds.
+        // WAS stable means: last build was worse or equal to FAILURE, second
+        // last was better than FAILURE
+        if (project.getBuilds() != null && project.getBuilds().size() >= 2) {
+            RunList<?> builds = project.getBuilds();
+            Run<?, ?> lastBuild = builds.getLastBuild();
+            Run<?, ?> secondLastBuild = builds.get(builds.size() - 2);
+            if (lastBuild != null && lastBuild.getResult() != null
+                    && lastBuild.getResult().isWorseOrEqualTo(Result.FAILURE)
+                    && secondLastBuild != null
+                    && secondLastBuild.getResult() != null
+                    && secondLastBuild.getResult().isBetterThan(Result.FAILURE)
+                    && !changesBetweenTwoBuilds(lastBuild, secondLastBuild)) {
+                // last build failed, but second one didn't and there were no
+                // changes between the two builds
+                // in this case we restart the build
+                return true;
+            }
+            // last build was not a failure or 2nd last was.
+            // no restart
+            return false;
+        }
+        // less than 2 builds
+        // no restart
+        return false;
+    }
 
-	/**
-	 * Searches for a given pattern in a given file.
-	 * 
-	 * @param abortAfterFirstHit
-	 *            normally true, can be set to false in order to continue
-	 *            searching.
-	 */
-	private boolean checkFile(File file, Pattern pattern,
-			boolean abortAfterFirstHit) {
-		if (pattern == null)
-			return false;
-		boolean rslt = false;
-		BufferedReader reader = null;
-		try {
-			reader = new BufferedReader(new FileReader(file));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				Matcher matcher = pattern.matcher(line);
-				if (matcher.find()) {
-					// we have a hit
-					rslt = true;
-					if (abortAfterFirstHit)
-						return true;
-				}
-			}
-		} catch (IOException e) {
-			LOGGER.info("File could not be read!");
-		} finally {
-			IOUtils.closeQuietly(reader);
-		}
-		return rslt;
-	}
+    /**
+     * Helper method for restarting a project.
+     * 
+     * @param project
+     *            the project.
+     * @param config
+     *            instance of periodic reincarnation configuration.
+     */
+    private void restart(Project<?, ?> project,
+            ReincarnateFailedJobsConfiguration config, String cause) {
+        project.scheduleBuild(new ReincarnateFailedBuildsCause());
+        if (config.isLogInfoEnabled()) {
+
+            LOGGER.info("Restarting failed build(" + cause + ")........"
+                    + project.getDisplayName());
+        }
+    }
+
+    /**
+     * Checks if a certain build matches any of the given regular expressions.
+     * 
+     * @param build
+     *            the build.
+     * @return true if at least one match, false otherwise.
+     */
+    private boolean checkRegExprs(Run<?, ?> build) {
+        ReincarnateFailedJobsConfiguration config = new ReincarnateFailedJobsConfiguration();
+        List<RegEx> regExprs = config.getRegExprs();
+        if (regExprs == null || regExprs.size() == 0)
+            return true;
+        for (Iterator<RegEx> i = regExprs.iterator(); i.hasNext();) {
+            RegEx currentRegEx = i.next();
+            try {
+                if (checkFile(build.getLogFile(), currentRegEx.getPattern(),
+                        true)) {
+                    return true;
+                }
+            } catch (AbortException e) {
+                // e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Searches for a given pattern in a given file.
+     * 
+     * @param abortAfterFirstHit
+     *            normally true, can be set to false in order to continue
+     *            searching.
+     */
+    private boolean checkFile(File file, Pattern pattern,
+            boolean abortAfterFirstHit) {
+        if (pattern == null)
+            return false;
+        boolean rslt = false;
+        BufferedReader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    // we have a hit
+                    rslt = true;
+                    if (abortAfterFirstHit)
+                        return true;
+                }
+            }
+        } catch (IOException e) {
+            LOGGER.info("File could not be read!");
+        } finally {
+            IOUtils.closeQuietly(reader);
+        }
+        return rslt;
+    }
 
 }
--- src/test/java/org/jenkinsci/plugins/TestConfiguration.java
+++ src/test/java/org/jenkinsci/plugins/TestConfiguration.java
@@ -11,6 +11,7 @@ import hudson.model.Result;
 import org.jenkinsci.plugins.ReincarnateFailedJobsConfiguration;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.jvnet.hudson.test.HudsonTestCase;
 import org.jvnet.hudson.test.recipes.LocalData;
 import org.jvnet.hudson.test.recipes.Recipe;
@@ -26,85 +27,85 @@ import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
 
 public class TestConfiguration extends HudsonTestCase {
 
-	private ReincarnateFailedJobsConfiguration config;
-	private HtmlForm form;
+    private ReincarnateFailedJobsConfiguration config;
+    private HtmlForm form;
 
-	@LocalData
-	public void test1() throws Exception {
-		
-		assertNotNull(PeriodicReincarnation.get());
-		assertEquals("PeriodicReincarnation", new ReincarnateFailedBuildsCause().getShortDescription());
-		assertEquals(60000, PeriodicReincarnation.get().getRecurrencePeriod());
-		
-		Job<?, ?> job1 = (Job<?, ?>) Hudson.getInstance().getItem("test_job");
-		assertNotNull("job missing.. @LocalData problem?", job1);
-		assertEquals(Result.FAILURE, job1.getLastBuild().getResult());
-		System.out.println("JOB1 LOG:" + job1.getLastBuild().getLogFile().toString());
-		
-		Job<?, ?> job2 = (Job<?, ?>) Hudson.getInstance().getItem("no_change");
-		assertNotNull("job missing.. @LocalData problem?", job2);
-		assertEquals(Result.FAILURE, job2.getLastBuild().getResult());
-		assertNotNull(job2.getLastSuccessfulBuild());
+    @LocalData
+    public void test1() throws Exception {
 
-		this.getGlobalForm();
-		
-		final HtmlTextInput cronTime = form.getInputByName("_.cronTime");
-		assertNotNull("Cron Time is null!", cronTime);
-		cronTime.setValueAttribute("* * * * *");
+        assertNotNull(PeriodicReincarnation.get());
+        assertEquals("PeriodicReincarnation",
+                new ReincarnateFailedBuildsCause().getShortDescription());
+        assertEquals(60000, PeriodicReincarnation.get().getRecurrencePeriod());
 
-		final HtmlInput active = form.getInputByName("_.active");
-		assertNotNull("EnableDisable field is null!", active);
-		active.setChecked(true);
+        Job<?, ?> job1 = (Job<?, ?>) Hudson.getInstance().getItem("test_job");
+        assertNotNull("job missing.. @LocalData problem?", job1);
+        assertEquals(Result.FAILURE, job1.getLastBuild().getResult());
+        System.out.println("JOB1 LOG:"
+                + job1.getLastBuild().getLogFile().toString());
 
-		final HtmlInput logInfo = form.getInputByName("_.logInfo");
-		assertNotNull("Log info checkbox is null!", logInfo);
-		logInfo.setChecked(true);
+        Job<?, ?> job2 = (Job<?, ?>) Hudson.getInstance().getItem("no_change");
+        assertNotNull("job missing.. @LocalData problem?", job2);
+        assertEquals(Result.FAILURE, job2.getLastBuild().getResult());
+        assertNotNull(job2.getLastSuccessfulBuild());
 
-		final HtmlInput noChange = form.getInputByName("_.noChange");
-		assertNotNull("NoChange checkbox was null!", noChange);
-		noChange.setChecked(true);
+        this.getGlobalForm();
 
-		final HtmlElement regExprs = (HtmlElement) form.getByXPath(
-				"//tr[td='Regular Expressions']").get(0);
-		assertNotNull("RegExprs is null!", regExprs);
-		assertNotNull("Add button not found",
-				regExprs.getFirstByXPath(".//button"));
-		((HtmlButton) regExprs.getFirstByXPath(".//button")).click();
-		final HtmlTextInput regEx1 = (HtmlTextInput) regExprs
-				.getFirstByXPath("//input[@name='" + "regExprs.value" + "']");
-		assertNotNull("regEx1 is null!", regEx1);
-		regEx1.setValueAttribute("reg ex one");
+        final HtmlTextInput cronTime = form.getInputByName("_.cronTime");
+        assertNotNull("Cron Time is null!", cronTime);
+        cronTime.setValueAttribute("* * * * *");
 
-		submit(form);
-		config = new ReincarnateFailedJobsConfiguration();
-		assertNotNull(config);
-		assertEquals("* * * * *", config.getCronTime());
-		assertEquals("true", config.getActive());
-		assertTrue(config.isActive());
-		assertEquals(1, config.getRegExprs().size());
-		assertEquals("reg ex one", config.getRegExprs().get(0).getValue());
-		assertEquals("true", config.getNoChange());
-		assertTrue(config.isRestartUnchangedJobsEnabled());
-		assertEquals("true", config.getLogInfo());
-		assertTrue(config.isLogInfoEnabled());
-	}
+        final HtmlInput active = form.getInputByName("_.active");
+        assertNotNull("EnableDisable field is null!", active);
+        active.setChecked(true);
 
-	private void getGlobalForm() throws IOException, SAXException {
-		HtmlPage page = new WebClient().goTo("configure");
-		final String allElements = page.asText();
+        final HtmlInput logInfo = form.getInputByName("_.logInfo");
+        assertNotNull("Log info checkbox is null!", logInfo);
+        logInfo.setChecked(true);
 
-		assertTrue(allElements.contains("Cron Time"));
-		assertTrue(allElements.contains("Periodic Reincarnation"));
-		assertTrue(allElements.contains("Regular Expressions"));
-		assertTrue(allElements.contains("Show Info in Log"));
-		assertTrue(allElements.contains("Enable/Disable"));
-		assertTrue(allElements
-				.contains("Restart unchanged builds failing for the first time"));
+        final HtmlInput noChange = form.getInputByName("_.noChange");
+        assertNotNull("NoChange checkbox was null!", noChange);
+        noChange.setChecked(true);
 
-		this.form = page.getFormByName("config");
-		assertNotNull("Form is null!", this.form);
-	}
-	
-	
+        final HtmlElement regExprs = (HtmlElement) form.getByXPath(
+                "//tr[td='Regular Expressions']").get(0);
+        assertNotNull("RegExprs is null!", regExprs);
+        assertNotNull("Add button not found",
+                regExprs.getFirstByXPath(".//button"));
+        ((HtmlButton) regExprs.getFirstByXPath(".//button")).click();
+        final HtmlTextInput regEx1 = (HtmlTextInput) regExprs
+                .getFirstByXPath("//input[@name='" + "regExprs.value" + "']");
+        assertNotNull("regEx1 is null!", regEx1);
+        regEx1.setValueAttribute("reg ex one");
+
+        submit(form);
+        config = new ReincarnateFailedJobsConfiguration();
+        assertNotNull(config);
+        assertEquals("* * * * *", config.getCronTime());
+        assertEquals("true", config.getActive());
+        assertTrue(config.isActive());
+        assertEquals(1, config.getRegExprs().size());
+        assertEquals("reg ex one", config.getRegExprs().get(0).getValue());
+        assertEquals("true", config.getNoChange());
+        assertTrue(config.isRestartUnchangedJobsEnabled());
+        assertEquals("true", config.getLogInfo());
+        assertTrue(config.isLogInfoEnabled());
+    }
+
+    private void getGlobalForm() throws IOException, SAXException {
+        HtmlPage page = new WebClient().goTo("configure");
+        final String allElements = page.asText();
+
+        assertTrue(allElements.contains("Cron Time"));
+        assertTrue(allElements.contains("Periodic Reincarnation"));
+        assertTrue(allElements.contains("Regular Expressions"));
+        assertTrue(allElements.contains("Show Info in Log"));
+        assertTrue(allElements.contains("Enable/Disable"));
+        assertTrue(allElements
+                .contains("Restart unchanged builds failing for the first time"));
+
+        this.form = page.getFormByName("config");
+        assertNotNull("Form is null!", this.form);
+    }
 
 }
