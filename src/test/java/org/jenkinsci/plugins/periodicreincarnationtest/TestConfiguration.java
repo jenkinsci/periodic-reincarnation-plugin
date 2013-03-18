0a7e6aff20fddf8d596978f75922a7c95eb7b7bc
boev <iordan.boev@gmail.com>
2013-03-18 16:28:59 +0100

    Better tests, now testing needs more time so that the asynchronous tasks can start their work.

--- src/test/java/org/jenkinsci/plugins/periodicreincarnationtest/TestConfiguration.java
+++ src/test/java/org/jenkinsci/plugins/periodicreincarnationtest/TestConfiguration.java
@@ -15,7 +15,6 @@ import org.jvnet.hudson.test.HudsonTestCase;
 import org.jvnet.hudson.test.recipes.LocalData;
 import org.xml.sax.SAXException;
 
-
 import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
@@ -41,7 +40,9 @@ public class TestConfiguration extends HudsonTestCase {
     private HtmlForm form;
 
     /**
-     * TestCase.
+     * TestCase for Periodic Reincarnation. Populates the configuration with
+     * values and submits it. Checks if the values have been populated right and
+     * waits for the PeriodicReincarnation to start restarting jobs.
      * 
      * @throws Exception
      *             exception.
@@ -57,21 +58,63 @@ public class TestConfiguration extends HudsonTestCase {
         assertEquals(reccurancePeriod, PeriodicReincarnation.get()
                 .getRecurrencePeriod());
 
-        final Job<?, ?> job1 = (Job<?, ?>) Hudson.getInstance().getItem(
-                "test_job");
-        assertNotNull("job missing.. @LocalData problem?", job1);
-        assertEquals(Result.FAILURE, job1.getLastBuild().getResult());
-        System.out.println("JOB1 LOG:"
-                + job1.getLastBuild().getLogFile().toString());
-
-        final Job<?, ?> job2 = (Job<?, ?>) Hudson.getInstance().getItem(
-                "no_change");
-        assertNotNull("job missing.. @LocalData problem?", job2);
-        assertEquals(Result.FAILURE, job2.getLastBuild().getResult());
-        assertNotNull(job2.getLastSuccessfulBuild());
+        checkJobsFromLocalData();
 
         this.getGlobalForm();
 
+        populateValues();
+
+        checkPopulatedValues();
+
+        // checkLocalConfiguration();
+
+        Thread.sleep(1000 * 60);
+    }
+
+    /**
+     * Method should test local config page, but is never called due to an error
+     * Error: EcmaError: lineNumber=[969] column=[0] lineSource=[null]
+     * name=[TypeError]
+     * sourceName=[http://localhost:50018/static/d01de31c/scripts
+     * /hudson-behavior.js] message=[TypeError: Cannot call method
+     * "hasClassName" of undefined
+     * (http://localhost:50018/static/d01de31c/scripts/hudson-behavior.js#969)]
+     * com.gargoylesoftware.htmlunit.ScriptException: TypeError: Cannot call
+     * method "hasClassName" of undefined
+     * (http://localhost:50018/static/d01de31c/scripts/hudson-behavior.js#969)
+     * 
+     * 
+     * @throws IOException
+     * @throws SAXException
+     * @throws Exception
+     */
+//    private void checkLocalConfiguration() throws IOException, SAXException,
+//            Exception {
+//        final HtmlPage page = new WebClient()
+//                .goTo("job/afterbuild_test/configure");
+//        final String allElements = page.asText();
+//        assertTrue(allElements
+//                .contains("Configure PeriodicReincarnation locally"));
+//        HtmlForm localForm = page.getFormByName("config");
+//        assertNotNull(localForm);
+//
+//        final HtmlInput isLocallyConfigured = localForm
+//                .getInputByName("isLocallyConfigured");
+//        assertNotNull("isLocallyConfigured checkbox was null!",
+//                isLocallyConfigured);
+//        isLocallyConfigured.setChecked(true);
+//
+//        final HtmlInput isEnabled = localForm.getInputByName("_.isEnabled");
+//        assertNotNull("isEnabled checkbox was null!", isEnabled);
+//        isEnabled.setChecked(true);
+//
+//        final HtmlTextInput maxDepth = localForm.getInputByName("_.maxDepth");
+//        assertNotNull("MaxDepth(local) field is null!", maxDepth);
+//        maxDepth.setValueAttribute("2");
+//        submit(localForm);
+//    }
+
+    private void populateValues() throws Exception {
         final HtmlTextInput cronTime = form.getInputByName("_.cronTime");
         assertNotNull("Cron Time is null!", cronTime);
         cronTime.setValueAttribute("* * * * *");
@@ -85,7 +128,7 @@ public class TestConfiguration extends HudsonTestCase {
         activeTrigger.setChecked(true);
 
         final HtmlTextInput maxDepth = form.getInputByName("_.maxDepth");
-        assertNotNull("EnableDisable trigger field is null!", maxDepth);
+        assertNotNull("MaxDepth field is null!", maxDepth);
         maxDepth.setValueAttribute("2");
 
         final HtmlInput noChange = form.getInputByName("_.noChange");
@@ -101,10 +144,29 @@ public class TestConfiguration extends HudsonTestCase {
         final HtmlTextInput regEx1 = (HtmlTextInput) regExprs
                 .getFirstByXPath("//input[@name='" + "regExprs.value" + "']");
         assertNotNull("regEx1 is null!", regEx1);
-        regEx1.setValueAttribute("reg ex one");
+        regEx1.setValueAttribute("test");
 
-        submit(form);
-        config = new PeriodicReincarnationGlobalConfiguration();
+        // submit all populated values
+        submit(this.form);
+    }
+
+    private void checkJobsFromLocalData() {
+        final Job<?, ?> job1 = (Job<?, ?>) Hudson.getInstance().getItem(
+                "test_job");
+        assertNotNull("job missing.. @LocalData problem?", job1);
+        assertEquals(Result.FAILURE, job1.getLastBuild().getResult());
+        System.out.println("JOB1 LOG:"
+                + job1.getLastBuild().getLogFile().toString());
+
+        final Job<?, ?> job2 = (Job<?, ?>) Hudson.getInstance().getItem(
+                "no_change");
+        assertNotNull("job missing.. @LocalData problem?", job2);
+        assertEquals(Result.FAILURE, job2.getLastBuild().getResult());
+        assertNotNull(job2.getLastSuccessfulBuild());
+    }
+
+    private void checkPopulatedValues() {
+        config = PeriodicReincarnationGlobalConfiguration.get();
         assertNotNull(config);
         assertEquals("* * * * *", config.getCronTime());
         try {
@@ -121,7 +183,7 @@ public class TestConfiguration extends HudsonTestCase {
         assertEquals("true", config.getActiveCron());
         assertEquals("true", config.getActiveTrigger());
         assertEquals(1, config.getRegExprs().size());
-        assertEquals("reg ex one", config.getRegExprs().get(0).getValue());
+        assertEquals("test", config.getRegExprs().get(0).getValue());
         assertEquals("true", config.getNoChange());
         assertTrue(config.isRestartUnchangedJobsEnabled());
         assertEquals(2, config.getMaxDepth());
@@ -143,13 +205,11 @@ public class TestConfiguration extends HudsonTestCase {
         assertTrue(allElements.contains("Cron Time"));
         assertTrue(allElements.contains("Periodic Reincarnation"));
         assertTrue(allElements.contains("Max restart depth"));
-        assertTrue(allElements
-                .contains("Enable/Disable afterbuild job reincarnation"));
+        assertTrue(allElements.contains("Enable afterbuild job reincarnation"));
         assertTrue(allElements.contains("Regular Expressions"));
+        assertTrue(allElements.contains("Enable cron job reincarnation"));
         assertTrue(allElements
-                .contains("Enable/Disable cron job reincarnation"));
-        assertTrue(allElements
-                .contains("Restart unchanged builds failing for the first time"));
+                .contains("Restart unchanged projects failing for the first time"));
 
         this.form = page.getFormByName("config");
         assertNotNull("Form is null!", this.form);
